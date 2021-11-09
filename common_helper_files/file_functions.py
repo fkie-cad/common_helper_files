493bcedb6b9266279720c6da2135a790a1de8fab
Marten Ringwelski <git@maringuu.de>
2021-11-09 13:17:19 +0100

    Use pathlib
    As pathlib.Path.__init__ accepts both a string and Path-like objects we can
    stay backward compatible and still accept strings.
    
    Also Two functions are deprecated since they don't provide much readability
    over the pathlib implementation.

--- common_helper_files/file_functions.py
+++ common_helper_files/file_functions.py
@@ -1,8 +1,11 @@
-import os
+import io
+from pathlib import Path
+from typing import Type, Union
+
 import bitmath
 
 
-def read_in_chunks(file_object, chunk_size=1024):
+def read_in_chunks(file_object: Type[io.BufferedReader], chunk_size=1024) -> bytes:
     '''
     Helper function to read large file objects iteratively in smaller chunks. Can be used like this::
 
@@ -12,7 +15,6 @@ def read_in_chunks(file_object, chunk_size=1024):
 
     :param file_object: The file object from which the chunk data is read. Must be a subclass of ``io.BufferedReader``.
     :param chunk_size: Number of bytes to read per chunk.
-    :type chunk_size: int
     :return: Returns a generator to iterate over all chunks, see above for usage.
     '''
     while True:
@@ -22,35 +24,36 @@ def read_in_chunks(file_object, chunk_size=1024):
         yield data
 
 
-def get_directory_for_filename(filename):
+def get_directory_for_filename(filename: Union[str, Path]) -> str:
     '''
     Convenience function which returns the absolute path to the directory that contains the given file name.
 
     :param filename: Path of the file. Can be absolute or relative to the current directory.
-    :type filename: str
     :return: Absolute path of the directory
+
+    .. deprecated::
+        You should use pathlib instead of this function.
+
     '''
-    return os.path.dirname(os.path.abspath(filename))
+    return str(Path(filename).resolve().parent)
 
 
-def create_dir_for_file(file_path):
+def create_dir_for_file(file_path: Union[str, Path]) -> None:
     '''
     Creates all directories of file path. File path may include the file as well.
 
     :param file_path: Path of the file. Can be absolute or relative to the current directory.
-    :type file_path: str
-    :return: None
+
+    .. deprecated::
+        You should use pathlib instead of this function.
     '''
-    directory = os.path.dirname(os.path.abspath(file_path))
-    os.makedirs(directory, exist_ok=True)
+    Path(file_path).resolve().parent.mkdir(parents=True, exist_ok=True)
 
 
-def human_readable_file_size(size_in_bytes):
+def human_readable_file_size(size_in_bytes: int) -> None:
     '''
     Returns a nicly human readable file size
 
     :param size_in_bytes: Size in Bytes
-    :type size_in_bytes: int
-    :return: str
     '''
     return bitmath.Byte(bytes=size_in_bytes).best_prefix().format('{value:.2f} {unit}')
